3683ab971c28d9a6ccffac46a154163f50af6c6c
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2022-12-08 00:17:31 +0700

    feat: fix presentation mode

--- config.el
+++ config.el
@@ -47,7 +47,7 @@
   (setq doom-modeline-hud nil
         doom-modeline-icon t
         doom-modeline-window-width-limit nil
-        doom-modeline-major-mode-icon t
+        doom-modeline-major-mode-icon nil
         doom-modeline-number-limit 99
         doom-modeline-lsp nil))
 
@@ -112,6 +112,7 @@
                         ("date"        . "date:       ")
                         ("author"      . "author:     ")
                         ("language"    . "language:   ")
+                        ("options"     . "options:    ")
                         (t . t)))
   (org-modern-block-fringe nil)
   :config
@@ -133,7 +134,7 @@
   `(org-block :inherit org-block :background ,(face-attribute 'default :background))
   `(org-block-begin-line :inherit shadow :height 0.8)
   `(org-meta-line :foreground ,(face-attribute 'shadow :foreground))
-  `(org-document-info :foreground ,(face-attribute 'default :foreground))
+  `(org-document-info :foreground ,(face-attribute 'shadow :foreground))
   `(org-document-title :height 2.0 :weight bold :foreground ,(face-attribute 'default :foreground)))
 
 (setq org-pretty-entities t
@@ -234,52 +235,110 @@
     (org-modern-mode 1)
     (setq-local face-remapping-alist nil)))
 
+(define-minor-mode +org-present-mode
+  "Toggle org-present-mode."
+  :lighter "+org-present-mode"
+  (if +org-present-mode
+      (org-present)
+    (org-present-quit)))
+
 (use-package! org-present
   :hook ((org-present-mode . +org-present-hook)
          (org-present-mode-quit . +org-present-quit-hook))
   :init
-  (map! :leader :desc "Present" "t p" #'org-present)
+  (map! :leader :desc "Present" "t p" #'+org-present-mode)
   (add-hook 'org-present-after-navigate-functions '+org-present-prepare-slide))
 
-(defvar +org-present-org-level-scale '((org-level-1 . 2.5)
-                                       (org-level-2 . 2.0)
-                                       (org-level-3 . 1.75)
-                                       (org-level-4 . 1.5)
+(defun org-present-add-overlays ()
+  "Add overlays for this mode."
+  (add-to-invisibility-spec '(org-present))
+  (save-excursion
+    ;; hide org-mode options starting with #+
+    (goto-char (point-min))
+    (while (re-search-forward "^[[:space:]]*\\(#\\+\\)\\([^[:space:]]+\\)[:space:]." nil t)
+      (let ((end (if (org-present-show-option (match-string 2)) 2 0)))
+        (org-present-add-overlay (match-beginning 1) (match-end end))))
+    ;; hide stars in headings
+    (if org-present-hide-stars-in-headings
+        (progn (goto-char (point-min))
+               (while (re-search-forward "^\\(*+\\)" nil t)
+                 (org-present-add-overlay (match-beginning 1) (match-end 1)))))
+    ;; hide emphasis/verbatim markers if not already hidden by org
+    (if org-hide-emphasis-markers nil
+      ;; TODO https://github.com/rlister/org-present/issues/12
+      ;; It would be better to reuse org's own facility for this, if possible.
+      ;; However it is not obvious how to do this.
+      (progn
+        ;; hide emphasis markers
+        (goto-char (point-min))
+        (while (re-search-forward org-emph-re nil t)
+          (org-present-add-overlay (match-beginning 2) (1+ (match-beginning 2)))
+          (org-present-add-overlay (1- (match-end 2)) (match-end 2)))
+        ;; hide verbatim markers
+        (goto-char (point-min))
+        (while (re-search-forward org-verbatim-re nil t)
+          (org-present-add-overlay (match-beginning 2) (1+ (match-beginning 2)))
+          (org-present-add-overlay (1- (match-end 2)) (match-end 2)))))))
+
+(defvar +org-present-org-level-scale '((org-level-1 . 2.0)
+                                       (org-level-2 . 1.75)
+                                       (org-level-3 . 1.5)
+                                       (org-level-4 . 1.25)
                                        (org-level-5 . 1.25)
-                                       (org-level-6 . 1.0)
-                                       (org-level-7 . 1.0)
-                                       (org-level-8 . 1.0))
+                                       (org-level-6 . 1.25)
+                                       (org-level-7 . 1.25)
+                                       (org-level-8 . 1.25))
   "Org level size remap for presentation.")
 
-;; TODO: Save previous org-indent-mode
-;;       Also for the org-modern-mode
+(defvar +org-present-original-org-modern-hide-stars nil)
+(defvar +org-present-original-org-indent-mode nil)
+(defvar +org-present-original-org-modern-keyword nil)
+(defvar +org-present-org-modern-keyword '(("title"       . "")
+                                          ("description" . "")
+                                          ("subtitle"    . "")
+                                          ("date"        . "")
+                                          ("author"      . "")
+                                          ("language"    . "")
+                                          ("options"     . "")
+                                          (t . t)))
+
 (defun +org-present-hook ()
+  (setq-local +org-present-original-org-modern-hide-stars org-modern-hide-stars
+              +org-present-original-org-indent-mode org-indent-mode
+              +org-present-original-org-modern-keyword org-modern-keyword)
   (org-modern-mode 0)
   (org-indent-mode 0)
-  (setq-local visual-fill-column-width 96
+  (setq-local visual-fill-column-width 128
               visual-fill-column-center-text t
               org-modern-hide-stars t
-              header-line-format " ")
+              header-line-format " "
+              org-modern-keyword +org-present-org-modern-keyword)
   (setq-local face-remapping-alist (append (mapcar (lambda (face) `(,(car face) (:height ,(cdr face))  ,(car face))) +org-present-org-level-scale)
-                                           '((default (:height 1.5) default)
-                                             (header-line (:height 4.0) header-line)
+                                           '((default (:height 1.25) default)
+                                             (header-line (:height 8.0) header-line)
                                              (org-document-title (:height 2.0) org-document-title)
-                                             (org-document-info (:height 1.5) org-document-info))))
+                                             (org-document-info (:height 1.0) org-document-info))))
   (display-line-numbers-mode 0)
   (visual-fill-column-mode 1)
   (visual-line-mode 1)
   (hide-mode-line-mode 1)
   (org-modern-mode 1)
   (mixed-pitch-mode 1)
+  (evil-normal-state)
   (org-display-inline-images))
 
-;; TODO: Restore original org-indent-mode value
-;;       Restore org-modern-hide-stars value
+(defun +org-present-prepare-slide (buffer-name heading)
+  (org-overview)
+  (org-fold-show-entry)
+  (org-fold-show-children))
+
 (defun +org-present-quit-hook ()
   (setq-local header-line-format nil
               face-remapping-alist nil
               org-adapt-indentation nil
-              org-modern-hide-stars " ")
+              org-modern-hide-stars +org-present-original-org-modern-hide-stars
+              org-indent-mode +org-present-original-org-indent-mode
+              org-modern-keyword +org-present-original-org-modern-keyword)
   (org-present-small)
   (visual-fill-column-mode 0)
   (org-indent-mode 1)
@@ -287,11 +346,6 @@
   (mixed-pitch-mode 0)
   (org-mode-restart)
   (org-remove-inline-images))
-
-(defun +org-present-prepare-slide (buffer-name heading)
-  (org-overview)
-  (org-fold-show-entry)
-  (org-fold-show-children))
 
 (use-package! org-roam
   :init
